0c40cc46a7a2db9a4d395ad747b8df349cd68f1b
Usama-Cookie <30409982+Usama-Cookie@users.noreply.github.com>
2017-07-24 21:42:29 +0500

    23July2017

---
+++ Introduction.py
@@ -0,0 +1,37 @@
+name = "\tUSAMA\t";         '''\t is used for adding tabs on both sides of the name 
+                                this is done for making striping easy to understand'''
+num1 = 2;
+num2 = 3;
+print(name);                #printing the name as it is declared
+print(name.title());        #printing name only first letter upper case
+print(name.lower());        #printing the name in lower case
+print(name.upper());        #printing the name in Upper case
+print(name.rstrip());       #printing the name while striping the empty space from right
+print(name.lstrip());       #printing the name while striping the empty space from left
+print(name.strip());        #printing the name while striping the empty space from oth sides
+print(num1 + num2);         #printing the sum of two numbers
+print(num1 - num2);         #printing the difference of two numbers
+print(num1 * num2);         #printing the multiplication result of two numbers
+print(num1 / num2);         #printing the division result of two numbers
+print(num1 ** num2);        '''using double multiplication sign makes the first 
+                            number base and the second number exponent of it'''
+print(num1 ** 0.5);         #printing the square root of num1
+print(num2 ** 0.5);         #printing the square root of num2
+
+firstname = "Muhammad Usama";
+lastname = "Khan";
+fullname = firstname + lastname;
+print(fullname);
+#For adding space between first name and last name
+fullname = firstname + " " + lastname;
+print(fullname);
+
+random = "I am lovin' it";
+print(random);
+random = "\tI\n\tam\n\tlovin'\n\tit";       ''''#\n is used for starting a new line just 
+                                                like we do by pressing enter key'''
+print(random);
+
+'''observe the result carefully and try to understand everything
+    That's it for today
+    THANKYOU'''
